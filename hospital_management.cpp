c87fbd4d2d3f1545b87b576902c4d9a202ff910c
DeepakKumarDharmaraj <dk10293864@gmail.com>
2026-07-06 13:32:54 +0530

    feat: conflict

--- hospital_management.cpp
+++ hospital_management.cpp
@@ -351,5 +351,7 @@ int main() {
 
     cout << "\nPersons after cleanup: " << Person::totalPersons << "\n";
     cout << "\n[System] Hospital shutdown complete.\n";
+
+    cout << "\ngit conflict" << endl;
     return 0;
 }
